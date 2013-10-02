990a5edb0fee37806da65eb70d47e42b36e712d8
New Media Works - Support <support@newmediaworks.com>
2013-10-02 07:06:03 +0000

    Added JSP EL function for getErrorReason

---
+++ src/com/newmediaworks/taglib/email/Functions.java
@@ -0,0 +1,35 @@
+/*
+ * new-email-taglib - Java taglib encapsulating the JavaMail API.
+ * Copyright (C) 2013  New Media Works
+ *     info@newmediaworks.com
+ *     PO BOX 853
+ *     Napa, CA 94559
+ *
+ * This file is part of nmw-email-taglib.
+ *
+ * nmw-email-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * nmw-email-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with nmw-email-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.newmediaworks.taglib.email;
+
+import static com.aoindustries.servlet.filter.FunctionContext.getRequest;
+
+final public class Functions {
+
+    private Functions() {
+    }
+
+	public static String getErrorReason() {
+		return (String)getRequest().getAttribute(EmailTag.ERROR_REQUEST_PARAMETER_NAME);
+	}
+}
