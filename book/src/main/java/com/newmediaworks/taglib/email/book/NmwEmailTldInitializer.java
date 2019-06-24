0062fbc8ea5a4f9207d704d0aa6d44425f46a7d7
New Media Works <oss@newmediaworks.com>
2019-06-24 15:31:17 -0500

    Removed "TODO" that need not be done.

--- book/src/main/java/com/newmediaworks/taglib/email/book/NmwEmailTldInitializer.java
+++ book/src/main/java/com/newmediaworks/taglib/email/book/NmwEmailTldInitializer.java
@@ -33,7 +33,7 @@ public class NmwEmailTldInitializer extends TagReferenceInitializer {
 		// Self
 		additionalApiLinks.put("com.newmediaworks.taglib.email.", Maven.properties.getProperty("documented.url") + "apidocs/");
 		// Dependencies
-		// TODO: additionalApiLinks.put("com.aoindustries.util.", "https://aoindustries.com/aocode-public/apidocs/");
+		// additionalApiLinks.put("com.aoindustries.util.", "https://aoindustries.com/aocode-public/apidocs/");
 	}
 
 	public NmwEmailTldInitializer() {
