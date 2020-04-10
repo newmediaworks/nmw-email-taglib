2f17411604f77b591719e9e65cfa16cd66f40607
New Media Works <oss@newmediaworks.com>
2020-04-10 17:44:34 -0500

    Fixed API links.

--- book/src/main/java/com/newmediaworks/taglib/email/book/NmwEmailTldInitializer.java
+++ book/src/main/java/com/newmediaworks/taglib/email/book/NmwEmailTldInitializer.java
@@ -43,7 +43,7 @@ public class NmwEmailTldInitializer extends TagReferenceInitializer {
 			"/email-taglib",
 			"/nmw-email.tld",
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
-			Maven.properties.getProperty("javadoc.link.javaee.6"),
+			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			additionalApiLinks
 		);
 	}
