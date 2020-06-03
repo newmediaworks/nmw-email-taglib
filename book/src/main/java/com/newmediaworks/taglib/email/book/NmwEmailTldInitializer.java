4659687aa4b656145556e6aac337f6e42ccfa963
New Media Works <oss@newmediaworks.com>
2020-06-03 17:18:22 -0500

    Now using String varargs instead of generic Map

--- book/src/main/java/com/newmediaworks/taglib/email/book/NmwEmailTldInitializer.java
+++ book/src/main/java/com/newmediaworks/taglib/email/book/NmwEmailTldInitializer.java
@@ -23,7 +23,6 @@
 package com.newmediaworks.taglib.email.book;
 
 import com.semanticcms.tagreference.TagReferenceInitializer;
-import java.util.Collections;
 
 public class NmwEmailTldInitializer extends TagReferenceInitializer {
 
@@ -37,7 +36,7 @@ public class NmwEmailTldInitializer extends TagReferenceInitializer {
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
 			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			// Self
-			Collections.singletonMap("com.newmediaworks.taglib.email", Maven.properties.getProperty("project.url") + "apidocs/")
+			"com.newmediaworks.taglib.email", Maven.properties.getProperty("project.url") + "apidocs/"
 		);
 	}
 }
